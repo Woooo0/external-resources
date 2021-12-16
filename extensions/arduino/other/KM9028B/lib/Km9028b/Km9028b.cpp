1f04c4aa5bf8720024620919f5581bbe85061153
woooo <hug_sheng@qq.com>
2021-12-16 11:18:47 +0800

    修改由于电机瞬态干扰造成的重启问题

--- extensions/arduino/other/KM9028B/lib/Km9028b/Km9028b.cpp
+++ extensions/arduino/other/KM9028B/lib/Km9028b/Km9028b.cpp
@@ -1,5 +1,6 @@
 #include "km9028b.h"
 
+int powerSw = 15;
 static bool clogFlag;
 unsigned long triggertime = 0;
 unsigned long delta = 200;
@@ -10,6 +11,7 @@ Km9028b::Km9028b()
   pinMode(2, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
+  pinMode(powerSw, INPUT_PULLUP);
   pinMode(22, OUTPUT);
   pinMode(21, OUTPUT);
   pinMode(23, OUTPUT);
@@ -19,7 +21,7 @@ Km9028b::Km9028b()
   digitalWrite(2, HIGH);
   digitalWrite(16, HIGH);
   xTaskCreate(ctrl, "ctrl", 2048, NULL, 5, NULL);
-  attachInterrupt(digitalPinToInterrupt(5), falling, RISING);
+  attachInterrupt(digitalPinToInterrupt(powerSw), falling, RISING);
 }
 
 void Km9028b::coulomb(void *parameter)
@@ -51,12 +53,13 @@ void Km9028b::ctrl(void *parameter)
   static bool chargeFlag;
   while (1)
   {
-    if (digitalRead(39) && dis_coulomb_handler == NULL && powerFlag && analogRead(36) < 2100)
+    if (digitalRead(39) && dis_coulomb_handler == NULL && powerFlag && analogRead(36) < 2500)
     {
+      if (dis_run_handler != NULL)
+      {
+        vTaskDelete(dis_run_handler);
+      }
       digitalWrite(23, LOW);
-      digitalWrite(21, LOW);
-      digitalWrite(16, HIGH);
-      digitalWrite(2, HIGH);
       xTaskCreate(coulomb, "coulomb", 1024, NULL, 3, &dis_coulomb_handler);
     }
     else if (!digitalRead(39))
@@ -70,24 +73,24 @@ void Km9028b::ctrl(void *parameter)
     }
     else if (!powerFlag && chargeFlag && digitalRead(39) && analogRead(36) > 3100)
     {
-      digitalWrite(22, HIGH);
+      digitalWrite(23, HIGH);
       chargeFlag = 0;
     }
 
-    if (digitalRead(12) && digitalRead(5))
+    if (digitalRead(12) && digitalRead(powerSw))
     {
       powerFlag = 1;
       closeFlag = 0;
     }
-    if (!digitalRead(5))
+    if (!digitalRead(powerSw))
     {
       vTaskDelay(1500 / portTICK_PERIOD_MS);
-      if (!digitalRead(5) && !powerFlag)
+      if (!digitalRead(powerSw) && !powerFlag)
       {
         digitalWrite(12, HIGH);
         digitalWrite(23, HIGH);
       }
-      while (!digitalRead(5) && powerFlag)
+      while (!digitalRead(powerSw) && powerFlag)
       {
         vTaskDelay(200 / portTICK_PERIOD_MS);
         closeFlag++;
@@ -104,7 +107,7 @@ void Km9028b::ctrl(void *parameter)
           digitalWrite(12, LOW);
           digitalWrite(22, LOW);
           digitalWrite(23, LOW);
-          detachInterrupt(digitalPinToInterrupt(5));
+          detachInterrupt(digitalPinToInterrupt(powerSw));
         }
       }
     }
@@ -114,28 +117,30 @@ void Km9028b::ctrl(void *parameter)
 
 void Km9028b::falling()
 {
+  Serial.println("kwkw");
   static int time = millis();
-  if (dis_coulomb_handler == NULL && (millis() - triggertime > delta))
+  if (millis() - triggertime > delta)
   {
     triggertime = millis();
     if (!digitalRead(12) || digitalRead(13) || (dis_run_handler == NULL && millis() - time > 200))
     {
       digitalWrite(12, HIGH);
       digitalWrite(21, HIGH);
-      digitalWrite(22, LOW);
       xTaskCreate(run, "run", 1024, NULL, 2, &dis_run_handler);
       digitalWrite(13, LOW);
       digitalWrite(2, LOW);
       digitalWrite(16, LOW);
       clogFlag = 1;
+      detachInterrupt(digitalPinToInterrupt(powerSw));
+      attachInterrupt(digitalPinToInterrupt(powerSw), falling, FALLING);
     }
-    else if (dis_run_handler != NULL)
-    {
-      // digitalWrite(23, HIGH);
-      // digitalWrite(21, LOW);
-      // digitalWrite(13, HIGH);
-      // ESP.restart();
-    }
+    else if (dis_run_handler != NULL && millis() - time > 200 && !digitalRead(powerSw))
+      {
+        digitalWrite(23, HIGH);
+        digitalWrite(21, LOW);
+        digitalWrite(13, HIGH);
+        ESP.restart();
+      }
   }
 }
 
